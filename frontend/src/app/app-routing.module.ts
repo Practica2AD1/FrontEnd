bf7b3ca5ddecc9f6ca15f2b6f6286653cb4dab2e
Pablo Osuna <2472932372001@ingenieria.usac.edu.gt>
2020-03-22 12:50:57 +0000

    Aceptando ambos cambios

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,20 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ContactoComponent } from './components/contacto/contacto.component';
-<<<<<<< HEAD
-import { InformacionComponent } from './components/informacion/informacion.component';
 
-const routes: Routes = [
-  { path: 'contacto', component: ContactoComponent },
-  { path: 'informacion', component: InformacionComponent }
-=======
+import { InformacionComponent } from './components/informacion/informacion.component';
 import { InicioComponent } from './components/inicio/inicio.component';
 
-
 const routes: Routes = [
+  { path: '', component: InicioComponent },
   { path: 'contacto', component: ContactoComponent },
-  { path: '', component: InicioComponent }
->>>>>>> feature/pestañaInicio-201503911
+  { path: 'informacion', component: InformacionComponent }
 ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -4,21 +4,15 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ContactoComponent } from './components/contacto/contacto.component';
-<<<<<<< HEAD
 import { InformacionComponent } from './components/informacion/informacion.component';
-=======
 import { InicioComponent } from './components/inicio/inicio.component';
->>>>>>> feature/pestañaInicio-201503911
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactoComponent,
-<<<<<<< HEAD
-    InformacionComponent
-=======
+    InformacionComponent,
     InicioComponent
->>>>>>> feature/pestañaInicio-201503911
   ],
   imports: [
     BrowserModule,
