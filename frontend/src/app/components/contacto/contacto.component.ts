61c4faa49bc61e0986ad421b4538c6bb480e8b58
Pablo Osuna <2472932372001@ingenieria.usac.edu.gt>
2020-03-23 18:51:43 +0000

    Modificacion vistas

--- frontend/src/app/components/contacto/contacto.component.ts
+++ frontend/src/app/components/contacto/contacto.component.ts
@@ -7,6 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactoComponent implements OnInit {
 
+  dev1 = {
+    nombre: "Juan Pablo Osuna de Leon",
+    correo: "juanpabloosuna1997@gmail.com"
+  }
+  dev2 = {
+    nombre: "Jhonatan Leonel López Santos",
+    correo: "jhony@gmail.com"
+  }
+  dev3 = {
+    nombre: "Diego Josue Berrios Gutierrez",
+    correo: "diego@gmail.com"
+  }
+  dev4 = {
+    nombre: "Carlos Luis Pablo Hernández Gramajo",
+    correo: "carlos@gmail.com"
+  }
+  dev5 = {
+    nombre: "Mario Obed Morales Guitz",
+    correo: "mario@gmail.com"
+  }
+
+  devs: any[] =  [ this.dev1,this.dev2,this.dev3,this.dev4,this.dev5]
+
   constructor() { }
 
   ngOnInit() {
